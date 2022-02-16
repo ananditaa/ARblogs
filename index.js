88e446e8f8ad930a80de8d8bee045c1e58fcb107
Muhammad Rumaan <96781150+muhammadrumaan@users.noreply.github.com>
2022-02-16 19:06:06 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-window.onscroll=function(){scrolling();}
-function scrolling(){
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var srcHeight = document.documentElement.scrollHeight   -document.documentElement.clientHeight;
-    var scrolled = (winScroll/srcHeight)*100;
-    document.querySelector('.progress-bar') .style.width=scrolled+'%';
-   
-}
