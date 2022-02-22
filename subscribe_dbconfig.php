760835b699ec1ed6402f2f0445209395ae45a9b3
Ananditaa <81854479+ananditaa@users.noreply.github.com>
2022-02-22 21:48:06 +0530

    Add files via upload

---
+++ subscribe_dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "subscribe_db";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if (!$conn) { // If Check Connection
+    die("<script>alert('Connection Failed.')</script>");
+}
+
+?>
